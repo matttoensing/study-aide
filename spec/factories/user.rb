0646125ef933a57ee8557ad495c3e96228c1c456
Matt Toensing <matthew.toensing@gmail.com>
2021-12-05 10:41:12 -0700

    Change password in user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-    password_digest { Faker::Internet.password(min_length: 8, max_length: 20) }
+    password { Faker::Internet.password(min_length: 8, max_length: 20) }
   end
 end
