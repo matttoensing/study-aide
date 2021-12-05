b2a635bd8ebd7764d7c38df288101fe9b82c297d
Matt Toensing <matthew.toensing@gmail.com>
2021-12-05 14:27:10 -0700

    Include expectations for edit and delete buttons

--- spec/features/questions/new_spec.rb
+++ spec/features/questions/new_spec.rb
@@ -24,6 +24,37 @@ RSpec.describe 'new questions' do
       within("#question-#{question.id}") do
         expect(page).to have_content('Which tree family is Acer?')
         expect(page).to have_content('Maple')
+        expect(page).to have_button('Edit Question')
+        expect(page).to have_button('Delete Question')
+        expect(page).to have_button('Edit Answer')
+        expect(page).to have_button('Delete Answer')
+      end
+
+      expect(page).to have_button('Finish Question Submissions')
+    end
+
+    it 'user can edit a question or an answer' do
+      user = create(:user)
+      quiz = create(:quiz, user: user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit "/quizzes/#{quiz.id}/questions/new"
+
+      expect(page).to have_content("Submit a Question the Answer for #{quiz.name}")
+      expect(page).to_not have_button('Finish Questions')
+
+      fill_in :question, with: 'Which tree family is Acer?'
+      fill_in :answer, with: 'Maple'
+      click_on 'Submit'
+
+      question = Question.last
+
+      expect(current_path).to eq("/quizzes/#{quiz.id}/questions/new")
+      expect(page).to have_content('Current Quiz Questions')
+
+      within("#question-#{question.id}") do
+        expect(page).to have_content('Which tree family is Acer?')
+        expect(page).to have_content('Maple')
         expect(page).to have_button('Edit')
         expect(page).to have_button('Delete')
       end
