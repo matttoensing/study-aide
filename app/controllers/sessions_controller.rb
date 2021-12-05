0df2d63d1a3db5956845716f3c5f1e344e528829
Matt Toensing <matthew.toensing@gmail.com>
2021-12-05 11:10:47 -0700

    Create sessions controller with create action for returning users

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if !user.nil? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.email}!"
+      redirect_to '/dashboard'
+    else
+      flash[:error] = 'Invalid Email or Password'
+      redirect_to '/'
+    end
+  end
+end
