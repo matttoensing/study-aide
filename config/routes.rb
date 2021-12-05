e950f5ec7445de6c99d7dcb6e828aafda36a54d8
Matt Toensing <matthew.toensing@gmail.com>
2021-12-05 11:07:44 -0700

    Add line breaks for easier reading

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
+  post '/login', to: 'sessions#create'
+  
   get '/register', to: 'users#new', as: 'register'
   post '/register', to: 'users#create'
+  get '/dashboard', to: 'users#show'
 end
