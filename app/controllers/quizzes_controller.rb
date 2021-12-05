4d8d0e2e7e723cdef148e51f10abfe6c6e6d17ad
Matt Toensing <matthew.toensing@gmail.com>
2021-12-05 16:33:49 -0700

    Create edit quiz functionality

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -16,6 +16,12 @@ class QuizzesController < ApplicationController
     redirect_to new_quiz_question_path(quiz.id)
   end
 
+  def update
+    quiz = Quiz.update(params[:id], quiz_params)
+
+    redirect_to "/quizzes/#{quiz.id}"
+  end
+
   def destroy
     quiz = Quiz.find(params[:id])
     quiz.destroy
@@ -23,9 +29,9 @@ class QuizzesController < ApplicationController
     redirect_to '/dashboard'
   end
 
-  # private
-  #
-  # def quiz_params
-  #   params.permit(:name, :subject, :description)
-  # end
+  private
+
+  def quiz_params
+    params.permit(:name, :subject, :description)
+  end
 end
---
+++ spec/features/quizzes/edit_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+ RSpec.describe 'edit quiz page' do
+   it 'displays a form to edit a quiz' do
+     user = create(:user)
+     quiz = create(:quiz, user: user)
+
+     visit "/quizzes/#{quiz.id}/edit"
+
+     fill_in :name, with: 'Botany'
+     fill_in :description, with: 'Rocky Mountain Flora Practice Quiz'
+     click_on 'Submit'
+
+     expect(current_path).to eq("/quizzes/#{quiz.id}")
+     expect(page).to have_content('Botany')
+     expect(page).to have_content('Rocky Mountain Flora Practice Quiz')
+     expect(page).to have_content(quiz.subject)
+   end
+ end
