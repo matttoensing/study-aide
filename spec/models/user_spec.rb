103ea52058b8bb843eeeec16493026becd8b7584
Matt Toensing <matthew.toensing@gmail.com>
2021-12-05 10:41:21 -0700

    Add user validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,11 @@ require 'rails_helper'
    describe 'relationships' do
      it { should have_many(:quizzes) }
    end
+
+   describe 'validations' do
+     it { should validate_presence_of(:email) }
+     it { should validate_uniqueness_of(:email) }
+     it { should validate_presence_of(:password)}
+     it { should validate_confirmation_of(:password)}
+   end
  end
