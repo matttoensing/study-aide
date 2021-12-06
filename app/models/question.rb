1b34fa957a49fc2915e9f2e1a1c60086f448f97b
Matt Toensing <matthew.toensing@gmail.com>
2021-12-05 18:40:16 -0700

    Add method to shuffle answers for a game question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,16 @@
 class Question < ApplicationRecord
   belongs_to :quiz
   has_one :answer, dependent: :destroy
+
+  def question_answers
+    answers = [self.answer]
+    quiz_answers = self.quiz.answers.shuffle
+    
+    until answers.length == 4
+      quiz_answers.each do |answer|
+        answers << answer
+      end
+    end
+    answers.shiffle
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,4 +5,27 @@ RSpec.describe Question do
     it { should belong_to(:quiz) }
     it { should have_one(:answer) }
   end
+
+  describe 'instance methods' do
+    describe '::question_answers' do
+      it 'creates a collection of answers for a question, with 3 being random from the whole quiz set and one being correct' do
+        user = create(:user)
+        quiz = create(:quiz, user: user)
+        question1 = create(:question, quiz: quiz)
+        answer1 = create(:answer, question: question1)
+        question2 = create(:question, quiz: quiz)
+        answer2 = create(:answer, question: question2)
+        question3 = create(:question, quiz: quiz)
+        answer3 = create(:answer, question: question3)
+        question4 = create(:question, quiz: quiz)
+        answer4 = create(:answer, question: question4)
+
+        expected = [answer2, answer3, answer4, answer1]
+
+        allow(question1).to receive(:question_answers).and_return(expected)
+
+        expect(question1.question_answers).to eq(expected)
+      end
+    end
+  end
 end
